27c0de97248cab7aaa2204ae490ea32d2fb050d4
Kyle Allie <kyledrallie@gmail.com>
2025-01-29 08:56:03 -0400

    Added and documented a copy of the cars dataset.

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Cars Dataset
+#'
+#' This dataset contains the speed of cars and the corresponding stopping distances.
+#'
+#' @format A data frame with 50 rows and 2 variables:
+#' \describe{
+#'   \item{speed}{A numeric value representing how fast the car was going (in mph).}
+#'   \item{dist}{A numeric value for how far the car traveled before stopping (in feet).}
+#' }
+#' @source Built-in R dataset.
+#' @examples
+#' data(cars)
+#' head(cars)
+#' @export
+"cars"
